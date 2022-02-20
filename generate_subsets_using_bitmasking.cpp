da3f5e20a18269a5e0635ae0df2c07fc44c772b6
OfficialAkashKumar <91385321+OfficialAkashKumar@users.noreply.github.com>
2022-02-20 10:10:33 +0530

    Update generate_subsets_using_bitmasking.cpp

--- generate_subsets_using_bitmasking.cpp
+++ generate_subsets_using_bitmasking.cpp
@@ -28,3 +28,41 @@ int main() {
   cin >> a;
   printsubsets(a);
 }
+
+
+// for strings
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll mod = 1e9 + 7;
+
+void filter(string s, ll n) {
+	for (int i = 0; i < (1 << s.size()); i++) {
+		if (n & 1) {
+			cout << s[i];
+		}
+		n >>= 1;
+	}
+	cout << endl;
+}
+
+void subsets(string s) {
+
+	for (ll i = 1; i <= (1 << s.size()); ++i) {
+		filter(s, i);
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	string s; cin >> s;
+	subsets(s);
+}
+https://codeforces.com/contest/1626/customtest
